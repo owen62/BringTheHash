e738324b6ba440297742efda15c5a526654b56e3
owen62 <65536127+owen62@users.noreply.github.com>
2023-06-14 18:30:17 +0200

    info() problem solved

--- main.py
+++ main.py
@@ -1,107 +1,78 @@
 import hashlib
 import pyfiglet
-import sys
 import getopt
+import sys
+import os
+import time
+
 
 def banner():
     ascii_banner = pyfiglet.figlet_format("BringMeTheHash")
     print(ascii_banner)
     print("Hash Algorithm available: MD5 | SHA1 | SHA224 | SHA256 | SHA384 | SHA512")
-    print ("Version 1.0 Made by Drackk")
+    print("Version 1.0 Made by Drackk")
+
 
 def info():
-    print ("")
-    print ("Information:")
-    print ("[*] Options:")
-    print ("[*] (-h) info")
-    print ("[*] (-v) value")
-    print ("[*] (-t) Type [See supported hashes]")
-    print ("[*] (-o) output")
-    print ("[*] Supported Hashes:")
-    print ("[>] md5, sha1, sha224, sha256, sha384, sha512")
-    print ("[*] That's all folks!\n")
-
-
-
-def CreatingHash(value, hashtype, verbose):
-    if "md5" in hashtype:
-        h = hashlib.md5(value.encode()).hexdigest()
-    elif "sha1" in hashtype:
-        h = hashlib.sha1(value.encode()).hexdigest()
-    elif "sha224" in hashtype:
-        h = hashlib.sha1(value.encode()).hexdigest()
-    elif "sha256" in hashtype:
-        h = hashlib.sha1(value.encode()).hexdigest()
-    elif "sha384" in hashtype:
-        h = hashlib.sha1(value.encode()).hexdigest()
-    elif "sha512" in hashtype:
-        h = hashlib.sha1(value.encode()).hexdigest()
-    else:
-        print("[-] I think this is not a supported hash type")
-        exit()
-
+    print("")
+    print("Information:")
+    print("[*] Options:")
+    print("[*] (-i) info")
+    print("[*] (-v) value")
+    print("[*] (-t) hashtype [See supported hashes]")
+    print("[*] (-o) output")
+    print("[*] Supported Hashes:")
+    print("[>] md5, sha1, sha224, sha256, sha384, sha512")
+    print("[*] That's all folks!\n")
 
 
 def start(argv):
     value = None
-    hashtype =None
-    verbose = None
+    hashtype = None
     banner()
+
     try:
-        opts, argv = getopt.getopt(argv, "h:v:t")
+        opts, args = getopt.getopt(argv, "ih:v:t:", ["info", "value=", "type="])
     except getopt.GetoptError:
-        print("[!!] Error on argument ")
+        print('[*] ./main.py -v <value> -t <type> ')
+        print('[*] Type ./main.py -i for information')
         sys.exit(1)
 
     for opt, arg in opts:
-        if opt == "-h":
+        if opt == ('-i'):
             info()
             sys.exit()
-        elif opt == ("-v","--value"):
+        elif opt in ("-v", "--value"):
             value = arg
-        elif opt == ("-t","--hashtype"):
+        elif opt in ["-t", "--type"]:
             hashtype = arg
-        else :
-            info()
-            sys.exit()
+
+    print(hashtype)
+    print(value)
+
+    if not (value and hashtype):
+        print('[*] ./main.py -v <value> -t <type>')
+        sys.exit()
+
 
 if __name__ == "__main__":
-    start(sys.argv)
+    start(sys.argv[1:])
 
 
 
-"""
-hash_type = input("Hash type desired : ")
-value = input("Value : ")
-
-if hash_type == "MD5":
-    encoded_string = value.encode()
-    hash_value = hashlib.md5(encoded_string).hexdigest()
-    print("MD5 : ", hash_value)
-
-elif hash_type == "SHA1":
-    encoded_string = value.encode()
-    hash_value = hashlib.sha1(encoded_string).hexdigest()
-    print("sha1 : ", hash_value)
-
-elif hash_type == "SHA224":
-    encoded_string = value.encode()
-    hash_value = hashlib.sha224(encoded_string).hexdigest()
-    print("sha224 : ", hash_value)
-
-
-elif hash_type == "SHA256":
-    encoded_string = value.encode()
-    hash_value = hashlib.sha256(encoded_string).hexdigest()
-    print("sha256 : ", hash_value)
-
-
-elif hash_type == "SHA512":
-    encoded_string = value.encode()
-    hash_value = hashlib.sha512(encoded_string).hexdigest()
-    print("sha512 : ", hash_value)
-
-else :
-    print(" This hash is not available ")
-
-"""
+def CreatingHash(value, hashtype):
+    if "md5" in hashtype:
+        v = hashlib.md5(value.encode()).hexdigest()
+    elif "sha1" in hashtype:
+        v = hashlib.sha1(value.encode()).hexdigest()
+    elif "sha224" in hashtype:
+        v = hashlib.sha1(value.encode()).hexdigest()
+    elif "sha256" in hashtype:
+        v = hashlib.sha1(value.encode()).hexdigest()
+    elif "sha384" in hashtype:
+        v = hashlib.sha1(value.encode()).hexdigest()
+    elif "sha512" in hashtype:
+        v = hashlib.sha1(value.encode()).hexdigest()
+    else:
+        print("[-] I think this is not a supported hash type")
+        exit()
